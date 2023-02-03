ec79e3cec6c395826de35ec0e35045b1b840ea86
Mariusz Wiśniowski <mariusz.wisniowski@capgemini.com>
2023-02-03 14:27:14 +0100

    Added saving dataframe to csv file.

--- codac.py
+++ codac.py
@@ -1,3 +1,4 @@
+"""Docstring for this module"""
 from pyspark.sql import SparkSession
 
 with open('/mnt/c/Users/marwisni/Downloads/codac_assignment_2023/dataset_one.csv',
@@ -7,9 +8,22 @@ with open('/mnt/c/Users/marwisni/Downloads/codac_assignment_2023/dataset_one.csv
 spark = SparkSession.builder.appName('My first spark experience').getOrCreate()
 
 print(spark.version)
-df = spark.read.csv(
+personal_data = spark.read.csv(
     '/mnt/c/Users/marwisni/Downloads/codac_assignment_2023/dataset_one.csv', header=True)
-type(df)
-df.show(5)
+print(type(personal_data))
+personal_data.show(5)
+
+financial_data = spark.read.csv(
+    '/mnt/c/Users/marwisni/Downloads/codac_assignment_2023/dataset_two.csv', header=True)
+print(type(financial_data))
+financial_data.show(5)
+
+joined_data = personal_data.join(financial_data, ['id'])
+for column in joined_data.columns:
+    print(column)
+joined_data.show(30)
+joined_data.write.csv(
+    '/mnt/c/Users/marwisni/Downloads/codac_assignment_2023/joined_data.csv', header=True)
+
 
 print('Hello World')

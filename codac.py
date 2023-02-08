9c073b095070a4a331ea501427b83ecd7c14919a
Mariusz Wiśniowski <mariusz.wisniowski@capgemini.com>
2023-02-08 15:25:35 +0100

    Refactored column_rename function.

--- codac.py
+++ codac.py
@@ -23,42 +23,37 @@ financial_data = financial_data.drop('cc_n')
 logging.info('Financial data has been filtered successfully.')
 
 joined_data = personal_data.join(financial_data, ['id'])
-logging.info(
-    'Personal and financial data has been joined together successfully.')
+logging.info('Personal and financial data has been joined together successfully.')
 
 
-def country_filter(df, countries_str: str):
+def country_filter(dataframe, countries_str: str):
     """TODO Docstring for function"""
     countries = [country.strip() for country in countries_str.split(',')]
-    return df.filter(df.country.isin(countries))
+    return dataframe.filter(dataframe.country.isin(countries))
 
 
 joined_data = country_filter(joined_data, sys.argv[3])
 logging.info('Data has been filtered by country successfully.')
 
-
-def column_rename(df, old_columns: list[str], new_columns: list[str]):
+changes = {
+    'id': 'client_identifier',
+    'btc_a': 'bitcoin_address',
+    'cc_t':  'credit_card_type'
+}
+def column_rename(dataframe, change: dict):
     """TODO docstring for function"""
-    if (len(old_columns) == len(new_columns)):
-        changes = {}
-        for i in range(len(old_columns)):
-            changes.update({old_columns[i]: f'{old_columns[i]} as {new_columns[i]}'})
-        changes_list = []
-        for column in df.columns:
-            if column in changes.keys():
-                changes_list.append(changes[column])
-            else:
-                changes_list.append(column)
-        return (changes_list)
-    else:
-        logging.error(
-            'Old columns name list and new columns name list have different length')
-        return None
+    changes_list = []
+    print(dataframe.columns)
+    for column in dataframe.columns:
+        print(column)
+        if column in change.keys():
+            changes_list.append(f"{column} as {change[column]}")
+        else:
+            changes_list.append(column)
+    return changes_list
 
-print(column_rename(joined_data, ['id', 'btc_a', 'cc_t'], ['client_identifier', 'bitcoin_address', 'credit_card_type']))
-changing_expression = ["id as client_identifier", "email",
-                       "country", "btc_a as bitcoin_address", "cc_t as credit_card_type"]
-joined_data = joined_data.selectExpr(changing_expression)
+
+joined_data = joined_data.selectExpr(column_rename(joined_data, changes))
 logging.info("Columns' names have been changed successfully.")
 joined_data.write.csv('./client_data', header=True, mode='overwrite')
 logging.info("Output file has been saved successfully.")

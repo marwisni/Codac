92aa18a876b17f0428ed5f87a2642a37b2401d1b
Mariusz Wiśniowski <mariusz.wisniowski@capgemini.com>
2023-02-21 12:35:47 +0100

    Added and modified documentation.

--- src/data_joiner/tools.py
+++ src/data_joiner/tools.py
@@ -1,5 +1,23 @@
+"""
+Module including auxiliary functions used in main module.
+
+Functions:
+    - column_rename(DataFrame, dict) -> DataFrame
+    - country_filter(DataFrame, str) -> DataFrame
+"""
+
+__docformat__ = 'restructuredtext'
+
 def column_rename(dataframe, change: dict):
-    # TODO Maybe should iterate by change not by colmns?
+    """Rename particular column names.
+    
+    Args:    
+        - dataframe (DataFrame): DataFrame for which columns names should be changed.        
+        - change (dict): Dictionary of changes that should happen in format "old_name": "new_name".
+
+    Returns:
+        DataFrame: Dataframe with renamed columns.
+    """
     changes_list = []
     for column in dataframe.columns:
         if column in change.keys():
@@ -10,8 +28,16 @@ def column_rename(dataframe, change: dict):
 
 
 def country_filter(dataframe, countries_str: str):
+    """Filter data from dataframe including only particular countries.
+
+    Args:
+        dataframe (DataFrame): Input DataFrame which should be filtered by country.
+        countries_str (str): Countries as comma separated string which should be included after filtering.
+
+    Returns:
+        DataFrame: Dataframe with data only from countries included in the countries_str.
+    """
     if countries_str == '':
         return dataframe
-    else:
-        countries = [country.strip() for country in countries_str.split(',')]
-        return dataframe.filter(dataframe.country.isin(countries))
+    countries = [country.strip() for country in countries_str.split(',')]
+    return dataframe.filter(dataframe.country.isin(countries))
